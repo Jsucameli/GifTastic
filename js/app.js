e3ee0857ad597b2ff8fa6d9a7e76566c984cdc09
J Suc <jsucameli87@gmail.com>
2019-03-30 22:49:14 -0400

    got the gifs from the api but have not gotten them to start and stop

--- js/app.js
+++ js/app.js
@@ -1,53 +1,66 @@
 var topics = ["Ford", "Nissan", "Saab", "Honda"];
 
-function getGifs(){
 
-var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=JGuKfVek4arxnvmcGckgh8t8msc1UUWb&limit=10&rating=R&lang=en$q=" + $(this).text()
+$(document).on("click", ".gif-btn", function() {
+// function getGifs(){
+ // $('#car-view').empty();
+selectedCar = this.id;
+console.log(selectedCar)
 
+// var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=JGuKfVek4arxnvmcGckgh8t8msc1UUWb&limit=10&rating=R&lang=en$q="
+// var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=JGuKfVek4arxnvmcGckgh8t8msc1UUWb&limit=10&q="+ selectedCar
+var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=JGuKfVek4arxnvmcGckgh8t8msc1UUWb&limit=10&q="+selectedCar
 $.ajax({
-  url: queryURL,
-  method: "GET",
+ url: queryURL,
+ method: "GET",
 })
-  .then(function (response) {
-    $("#car-view").empty();
-    var results = response.data;
+ .then(function (response) {
+   $("#car-view").empty();
+   var results = response.data;
 
-    console.log(response.data);
+   // console.log(response.data);
 
-    for (var i = 0; i < results.length; i++) {
-      var rating = results[i].rating;
-      rating = ("<p>").text("Rated: " + rating);
-      newDiv = ("<div");
-      gifImage = ("<img>");
-      gifImage.attr("src", results[i].images.original.url);
-      newDiv.append(gifImage);
-      $("#car-view").append(newDiv);
-    }
-  });
-}
+   for (var i = 0; i < results.length; i++) {
+     // var rating = results[i].rating;
+     // rating = ("<p>").text("Rated: " + rating);
+     // newDiv = ("<div");
+     // gifImage = ("<img>");
+     // gifImage.attr("src", results[i].images.original.url);
+     // newDiv.append(gifImage);
+     // $("#car-view").append(newDiv);
+     console.log(results[i].images.fixed_height_small_still.url)
+     // gifImage = ("<img>");
+     // gifImage.attr("id", selectedCar-i);
+     // gifImage.attr("src", results[i].images.fixed_height_small_still.url);
+     gifImage = "<img class='aniGIF' id='"+results[i].images.fixed_height_small_still.url+"' src='"+results[i].images.fixed_height_small_still.url+"'>"
+     $("#car-view").append(gifImage);
+   }
+ });
+});
 
 function newButtons() {
 
-  $("#button-view").empty();
+ $("#button-view").empty();
 
-  for(var i = 0; i < topics.length; i++) {
+ for(var i = 0; i < topics.length; i++) {
 
-    var newBtn = $("<button>");
-    newBtn.addClass("gif-btn");
-    newBtn.attr("data", topics[i]);
-    newBtn.text(topics[i]);
-    $("#button-view").prepend(newBtn);
+   var newBtn = $("<button>");
+   newBtn.addClass("gif-btn");
+   newBtn.attr("id", topics[i]);
+   newBtn.attr("data", topics[i]);
+   newBtn.text(topics[i]);
+   $("#button-view").prepend(newBtn);
 
-  };
+ };
 }
 
 $("#add-car").on("click", function(event){
-  event.preventDefault();
-  var newCar = $("#car-input").val().trim();
-  topics.push(newCar);
-  newButtons();
+ event.preventDefault();
+ var newCar = $("#car-input").val().trim();
+ topics.push(newCar);
+ newButtons();
 })
 
-$(document).on("click", ".gif-btn", getGifs);
+// $(document).on("click", ".gif-btn", getGifs);
 
 newButtons();
